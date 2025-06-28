82b0b0a0c65a3e27e7db8cad21fc9568121b00c2
tunckiral <tunc.kiral@gmail.com>
2025-06-28 05:14:52 -0400

    Fix empty link for Vercel in App component

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://vercel.com" target="_blank">
+        <a href="https://vercel.com" target="_blank"></a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
